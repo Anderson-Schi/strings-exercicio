4998ebc7c809884041b06ee545f70463b63282a6
Anderson-Schi <jr93@hotmai.com>
2022-10-28 23:05:28 +0100

    Exercicio 2 resolvido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,17 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = 'Eu não sou supersticioso, mas sou um pouco ________.      ';
+const result = minhaString.trim()
+const novaString = minhaString.replace("________","sticioso");
+console.log("Frase original: ", minhaString)
+console.log("Antes: ",minhaString.length,"caracteres");
+console.log("Depois: ",result.length,"caracteres");
+console.log("Substituido: ", novaString);
+
+
+
+
+
+
+
+
+
+
